76196a739a403402c7842d8cf5b426c787070d47
Nathan Osman <nathan@quickmediasolutions.com>
2020-03-28 14:32:06 -0700

    Add header component.

---
+++ ui/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="container">
+        <h1>Guessing Game</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
