0a94ca6f68532a47c3b48d7326e5a1eb2016bc8e
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-01 11:48:37 -0700

    Create store in root-level component.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.scss';
 import App from './App';
+import reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
