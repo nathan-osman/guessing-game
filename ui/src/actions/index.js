92e946f7449426d3aa3ed55a2337cc9e127f3abd
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-01 21:01:37 -0700

    Add a new action for creating a game.

--- ui/src/actions/index.js
+++ ui/src/actions/index.js
@@ -1,11 +1,26 @@
 import {
+  CLEAR_ERROR,
   SET_GAMES,
-  CLEAR_ERROR
+  CREATE_GAME
 } from "../constants/actionTypes";
 
 export const clearError = () => ({ type: CLEAR_ERROR });
 
 export const loadGames = () => ({
   type: SET_GAMES,
-  url: '/api/games'
+  url: '/api/games',
+  errorMessage: "Unable to load the list of games."
+});
+
+export const createGame = (name) => ({
+  type: CREATE_GAME,
+  url: '/api/create',
+  data: {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name })
+  },
+  errorMessage: "Unable to create a new game."
 });
