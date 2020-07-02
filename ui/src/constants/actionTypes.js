3e2de17f8a63cbc67ee4919ce444eb99ddbc713a
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-01 21:03:01 -0700

    Add missing constant.

--- ui/src/constants/actionTypes.js
+++ ui/src/constants/actionTypes.js
@@ -4,6 +4,7 @@ export const SET_ERROR = 'SET_ERROR';
 export const CLEAR_ERROR = 'CLEAR_ERROR';
 
 export const SET_GAMES = 'SET_GAMES';
+export const CREATE_GAME = 'CREATE_GAME';
 
 export const ADD_PLAYER = 'ADD_PLAYER';
 export const REMOVE_PLAYER = 'REMOVE_PLAYER';
